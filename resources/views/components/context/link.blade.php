4c65b271d0227eb0d3dd0e052e70f629f05c54a4
Reinhold-Jesse <reinhold.jesse@gmx.de>
2022-10-07 17:00:20 +0200

    update 07.10.2022

---
+++ resources/views/components/context/link.blade.php
@@ -0,0 +1,6 @@
+<li>
+    <a href="{{ $link }}"
+        class="flex px-4 py-2 text-gray-600 no-underline transition-colors duration-100 hover:text-primary-500 hover:no-underline"
+        @click.prevent="open=false">{{ $title }}</a>
+
+</li>
