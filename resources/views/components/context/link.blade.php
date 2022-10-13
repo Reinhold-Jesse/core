b39e1666a5fd308ab187dd0b4f06084450db44fe
Reinhold-Jesse <reinhold.jesse@gmx.de>
2022-10-13 18:15:52 +0200

    add optional anker or button

--- resources/views/components/context/link.blade.php
+++ resources/views/components/context/link.blade.php
@@ -1,6 +1,18 @@
-<li>
-    <a href="{{ $link }}"
-        class="flex px-4 py-2 text-gray-600 no-underline transition-colors duration-100 hover:text-primary-500 hover:no-underline"
-        @click.prevent="open=false">{{ $title }}</a>
+@props([
+    'link' => null,
+    'title' => null,
+])
 
-</li>
+@if (isset($link) && !empty($link))
+    <li>
+        <a href="{{ $link }}"
+            class="flex px-4 py-2 text-gray-600 no-underline transition-colors duration-100 hover:text-primary-500 hover:no-underline"
+            @click.prevent="open=false">{{ $title }}</a>
+
+    </li>
+@else
+    <li>
+        <button type="button" @click.prevent="open=false" {{ $attributes }}
+            class="flex px-4 py-2 text-gray-600 no-underline transition-colors duration-100 hover:text-primary-500 hover:no-underline">{{ $title }}</button>
+    </li>
+@endif
