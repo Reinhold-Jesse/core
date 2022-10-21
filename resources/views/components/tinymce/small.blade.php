6bc147194df91eaea123aa634f2803257e5c8a88
Reinhold-Jesse <reinhold.jesse@gmx.de>
2022-10-21 09:54:58 +0200

    Fixe tinymce after reload

--- resources/views/components/tinymce/small.blade.php
+++ resources/views/components/tinymce/small.blade.php
@@ -1,7 +1,9 @@
 @props(['value' => false])
 
-<textarea rows="5" cols="5"
-    {{ $attributes->merge(['class' => 'tinymceEditorSmall outline-primary-300 py-3 px-5 w-full border-primary-300 rounded-md focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-70']) }}>{{ $value }}</textarea>
+<div wire:ignore>
+    <textarea rows="5" cols="5"
+        {{ $attributes->merge(['class' => 'tinymceEditorSmall outline-primary-300 py-3 px-5 w-full border-primary-300 rounded-md focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-70']) }}>{{ $value }}</textarea>
+</div>
 
 @once
     @vite('resources/js/tinymce.js')
