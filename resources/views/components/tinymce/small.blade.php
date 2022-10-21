60707261f6b134e8a49d7288cd6798c547b4dced
Reinhold-Jesse <reinhold.jesse@gmx.de>
2022-10-21 09:39:53 +0200

    update css tinymce

--- resources/views/components/tinymce/small.blade.php
+++ resources/views/components/tinymce/small.blade.php
@@ -1,5 +1,7 @@
-<textarea class="tinymceEditorSmall"></textarea>
+@props(['value' => false])
 
+<textarea rows="5" cols="5"
+    {{ $attributes->merge(['class' => 'tinymceEditorSmall outline-primary-300 py-3 px-5 w-full border-primary-300 rounded-md focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-70']) }}>{{ $value }}</textarea>
 
 @once
     @vite('resources/js/tinymce.js')
