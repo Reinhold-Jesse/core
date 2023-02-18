2add7320c2430a52d8b6565174da8d3d23555fb8
Reinhold Jesse <reinhold.jesse@gmx.de>
2023-02-18 18:26:41 +0100

    add table footer

--- resources/views/components/table/wrapper.blade.php
+++ resources/views/components/table/wrapper.blade.php
@@ -5,4 +5,10 @@
     <tbody {{ $body->attributes->merge(['class' => 'bg-white divide-y divide-gray-200']) }}>
         {{ $body }}
     </tbody>
+    @if(isset($foot))
+    <tfoot>
+        {{$foot}}
+    </tfoot>
+    @endif
+
 </table>
